dc7b866b4def89e6229edd44bc1e47ef6c202573
fhasnur <fandi.hasnur10@gmail.com>
2024-09-20 23:02:30 +0700

    chore: remove unnecessary lines

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { languageOptions: { globals: globals.browser } },
